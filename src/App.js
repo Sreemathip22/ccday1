361e1da0ce74209b4c1f6c6c9b270aaa1c1f9c5a
Sreemathip22 <sreemathi.paramasivam22@gmail.com>
2023-04-05 15:29:47 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import "./App.css";
+import List from "./List";
+import AddChocolate from "./AddChocolate";
+import { maxBy } from "lodash";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chocolates: [
+        {
+          id: 1,
+          name: "Diary Milk",
+          brand: "Cadbury",
+          size: "small",
+          price: "5",
+          soh: 10
+        },
+        {
+          id: 2,
+          name: "5 Star",
+          brand: "Cadbury",
+          size: "small",
+          price: "5",
+          soh: 10
+        },
+        {
+          id: 3,
+          name: "KitKat",
+          brand: "Nestle",
+          size: "small",
+          price: "8",
+          soh: 4
+        }
+      ]
+    };
+    this.handleChocolateAdd = this.handleChocolateAdd.bind(this);
+    this.handleChocolateSale = this.handleChocolateSale.bind(this);
+  }
+
+  handleChocolateAdd(chocolate) {
+    let newId = maxBy(this.state.chocolates, "id");
+    let chocolates = this.state.chocolates.slice();
+    chocolate["id"] = newId;
+    chocolates.push(chocolate);
+    this.setState({ chocolates });
+  }
+
+  handleChocolateSale(id) {
+    var chocolates = [];
+    this.state.chocolates.forEach((ele, index, arr) => {
+      if (ele.id === id) {
+        ele.soh -= 1;
+      }
+      chocolates.push(ele);
+    });
+    this.setState({ chocolates });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI-JaZoP762HuJG0O09xTOLZsSHwsuwQN5Ow&usqp=CAU" className="App-logo" alt="logo" />
+          <h2>Welcome to Chocolate World</h2>
+        </div>
+        <AddChocolate onChocolateAdd={this.handleChocolateAdd} />
+        <List
+          chocolates={this.state.chocolates}
+          onChocolateSale={this.handleChocolateSale}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
